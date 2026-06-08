c67ea90330fe8a07f71c6fd03e459403e36af272
JackDaWack <jackson.s.netter49@gmail.com>
2026-06-08 18:56:15 -0400

    -Added skeleton base functions to rag_pipeline.py for later implementation.

--- app/rag_pipeline.py
+++ app/rag_pipeline.py
@@ -11,3 +11,16 @@ path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "data", "do
 
 if not os.path.exists(path):
     raise FileNotFoundError(f"Data directory not found: {path}")
+
+#Data Ingestion Functions.
+def load_documents():
+    pass
+
+def chunk():
+    pass
+
+def gen_embeds():
+    pass
+
+def store_vectors():
+    pass
