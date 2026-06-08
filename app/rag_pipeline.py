18162a383b58e0b3a1a4811aaa4108b0abfc8634
JackDaWack <jackson.s.netter49@gmail.com>
2026-06-08 19:00:49 -0400

    -Added skeleton base functions for query and answer handling to rag_pipeline.py.

--- app/rag_pipeline.py
+++ app/rag_pipeline.py
@@ -12,7 +12,7 @@ path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "data", "do
 if not os.path.exists(path):
     raise FileNotFoundError(f"Data directory not found: {path}")
 
-#Data Ingestion Functions.
+#Data Ingestion Functions:
 def load_documents():
     pass
 
@@ -23,4 +23,18 @@ def gen_embeds():
     pass
 
 def store_vectors():
+    pass
+
+#Query Handling Functions:
+def query_to_embeds():
+    pass
+
+def retrieve_candidates():
+    pass
+
+#Response Generation:
+def build_context():
+    pass
+
+def answer_query():
     pass
