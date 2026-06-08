e753644ebe09710bc01233f9e6a921e0538fe1e8
JackDaWack <jackson.s.netter49@gmail.com>
2026-06-08 18:46:56 -0400

    -Created new rag_pipeline.py file to be where the rag pipeline processes are implemented. -Added OpenAI api and libraries to rag_pipeline.py.

---
+++ app/rag_pipeline.py
@@ -0,0 +1,13 @@
+import os
+import json
+import torch
+import pdfplumber
+from openai import OpenAI
+import tiktoken
+
+open_ai = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "data", "documents"))
+
+if not os.path.exists(path):
+    raise FileNotFoundError(f"Data directory not found: {path}")
